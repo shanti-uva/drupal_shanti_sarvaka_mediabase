9154698187397f97ffe9f5ecc8049e981083a735
ThanGrove <than@virginia.edu>
2014-10-14 07:33:18 -0400

    update to av node form processing and handling.

--- template.php
+++ template.php
@@ -5,7 +5,27 @@
  * template.php
  */
  
- /**
-  *   This is the template.php file for a child sub-theme of the Shanti Sarvaka theme.
-  *   Use it to implement custom functions or override existing functions in the theme. 
-  */ 
+function sarvaka_mediabase_theme() {
+  return array(
+    'audio_node_form' => array(
+      'render element' => 'form',
+      'template' => 'av-node-form',
+      'path' => drupal_get_path('theme', 'sarvaka_mediabase') . '/templates'
+    ),
+    'video_node_form' => array(
+      'render element' => 'form',
+      'template' => 'av-node-form',
+      'path' => drupal_get_path('theme', 'sarvaka_mediabase') . '/templates'
+    ),
+  );
+}
+
+function sarvaka_mediabase_preprocess_video_node_form(&$vars) {
+	/*dpm($vars['form']);
+	$vars['messages'] = theme_status_messages(array('display' => 'status'));*/
+	
+}
+
+function sarvaka_mediabase_preprocess_audio_node_form(&$vars) {
+	// Add variables for audio node form processing here
+}
---
+++ templates/av-node-form.tpl.php
@@ -0,0 +1,22 @@
+<?php
+  /**
+	 * av-node-form.tpl.php: a template for audio and video node edit forms
+	 * 
+	 * called from sarvaka_mediabase_theme for both audio and video nodes
+	 * 
+	 * variables adjusted by the following functions:
+	 *     sarvaka_mediabase_preprocess_video_node_form()
+	 *     sarvaka_mediabase_preprocess_audio_node_form()
+	 * 
+	 * Variables:
+	 *    $form: The form itself
+	 * 
+	 * 
+	 */
+?>
+<div class="messages"><?php print $messages; ?></div>
+<div class="mb-av-form">
+	<?php
+		echo drupal_render_children($form); 
+	?>
+</div>
