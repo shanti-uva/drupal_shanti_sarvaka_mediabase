e0eb12322685df47d8c4d30ec2cfb269c94217c6
Edward Garrett <edward@pinedrop.com>
2014-11-26 22:03:33 +0000

    renamed and fixed some functions to use Transcripts UI and Transcripts Apache Solr

--- template.php
+++ template.php
@@ -155,28 +155,9 @@ function sarvaka_mediabase_select($vars) {
 }
 
 /**
- * Change styles applied to transcript search results.
- */
-function sarvaka_mediabase_preprocess_apachesolr_search_snippets(&$vars) {
-        if ($vars['doc']->entity_type == 'tcu') {
-                $button = '<div class="btn-group btn-group-lg btn-group-justified btn-group-transcript">';
-                $button .= '<div class="btn-group">';
-                $button .= '<button class="btn btn-default btn-icon">';
-                $button .= '<i class="icon shanticon-play-video"></i>';
-                $button .= '</button>';
-                $button .= '</div>';
-                $button .= '</div>';
-                $vars['transcripts_search_snippet']['#linktext'] = $button;
-                $vars['transcripts_search_snippet']['#attached'] = array(
-                        'css' => array(drupal_get_path('theme', 'sarvaka_mediabase') .'/css/transcripts-search-snippet.css'),
-                );
-        }
-}
-
-/**
  * Add js for play transcript button toggle
  */
-function sarvaka_mediabase_preprocess_transcripts_video_controls($vars) {
+function sarvaka_mediabase_preprocess_transcripts_ui_video_controls($vars) {
         drupal_add_js("
                 (function ($) {
                         $('.play-transcript').click(function() {
@@ -191,8 +172,17 @@ function sarvaka_mediabase_preprocess_transcripts_video_controls($vars) {
                 }(jQuery));
         ", 'inline');
 }
-
-function sarvaka_mediabase_transcripts_video_controls($vars) {
+function sarvaka_mediabase_transcripts_ui_goto_tcu($vars) {
+        $mins = floor ($vars['element']['#time'] / 60);
+        $secs = $vars['element']['#time'] % 60;
+        $time = sprintf ("%d:%02d", $mins, $secs);
+        $out = "<a href='" . $vars['element']['#linkurl'] . "' class='btn btn-default btn-sm'>";
+        $out .= "<span class='glyphicon glyphicon-play'></span> ";
+        $out .= $time;
+        $out .= "</a>";
+        return $out;
+}
+function sarvaka_mediabase_transcripts_ui_video_controls($vars) {
         $out = "<div style='width: 480px;' class='btn-group btn-group-lg btn-group-justified btn-group-transcript'>";
         $out .= "<div class='btn-group'>" .$vars['element']['#play']. "</div>";
         $out .= "<div class='btn-group'>" .$vars['element']['#prev']. "</div>";
@@ -201,41 +191,41 @@ function sarvaka_mediabase_transcripts_video_controls($vars) {
         $out .= "</div>";
         return $out;
 }
-function sarvaka_mediabase_transcripts_play_transcript($vars) {
+function sarvaka_mediabase_transcripts_ui_play_transcript($vars) {
         $out = "<button class='btn btn-primary btn-icon play-transcript without-transcript'>";
         $out .= "<i class='icon shanticon-play-video'></i>";
         $out .= "<span>" . t('Hide<br/>transcript') . "</span>";
         $out .= "</button>";
         return $out;
 }
-function sarvaka_mediabase_transcripts_previous_tcu($vars) {
+function sarvaka_mediabase_transcripts_ui_previous_tcu($vars) {
         $out = "<button class='btn btn-default btn-icon previous' title='Previous line'>";
         $out .= "<i class='icon shanticon-arrow-left'></i>";
         $out .= "</button>";
         return $out;
 }
-function sarvaka_mediabase_transcripts_same_tcu($vars) {
+function sarvaka_mediabase_transcripts_ui_same_tcu($vars) {
         $out = "<button class='btn btn-default btn-icon sameagain' title='Same line'>";
         $out .= "<i class='icon shanticon-spin3'></i>";
         $out .= "</button>";
         return $out;
 }
-function sarvaka_mediabase_transcripts_next_tcu($vars) {
+function sarvaka_mediabase_transcripts_ui_next_tcu($vars) {
         $out = "<button class='btn btn-default btn-icon next' title='Next line'>";
         $out .= "<i class='icon shanticon-arrow-right'></i>";
         $out .= "</button>";
         return $out;
 }
-function sarvaka_mediabase_form_transcripts_controller_mode_selector_alter(&$form, &$form_state) {
+function sarvaka_mediabase_form_transcripts_ui_mode_selector_alter(&$form, &$form_state) {
         $form['mode_selector']['#title'] = '';
         $form['mode_selector']['#attributes']['data-header'] = t('Select a transcript view');
-        $form['#attached']['css'][] = drupal_get_path('theme', 'sarvaka_mediabase') .'/css/transcripts-mode-selector.css';
-        $form['#attached']['js'][] = drupal_get_path('theme', 'sarvaka_mediabase') .'/js/transcripts-mode-selector.js';
+        $form['#attached']['css'][] = drupal_get_path('theme', 'sarvaka_mediabase') .'/css/transcripts-ui-mode-selector.css';
+        $form['#attached']['js'][] = drupal_get_path('theme', 'sarvaka_mediabase') .'/js/transcripts-ui-mode-selector.js';
 }
-function sarvaka_mediabase_form_transcripts_controller_tier_selector_alter(&$form, &$form_state) {
+function sarvaka_mediabase_form_transcripts_ui_tier_selector_alter(&$form, &$form_state) {
         $form['tier_selector']['#title'] = '';
         $form['tier_selector']['#attributes']['multiple'] = '';
         $form['tier_selector']['#attributes']['data-header'] = t('Choose languages to display');
         $form['tier_selector']['#attributes']['data-selected-text-format'] = 'count > 2';
-        $form['#attached']['css'][] = drupal_get_path('theme', 'sarvaka_mediabase') .'/css/transcripts-tier-selector.css';
+        $form['#attached']['css'][] = drupal_get_path('theme', 'sarvaka_mediabase') .'/css/transcripts-ui-tier-selector.css';
 }
