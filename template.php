9192e6a617e8708947ec3525e6d65a55ed17ffbf
Edward Garrett <edward@pinedrop.com>
2015-01-25 11:40:04 +0000

    added link_tcu and play_tcu templates

--- template.php
+++ template.php
@@ -334,7 +334,20 @@ function sarvaka_mediabase_transcripts_ui_transcript_search($vars) {
         $out = drupal_render($vars['element']['search_form']);
         return $out;
 }
-function sarvaka_mediabase_transcripts_ui_goto_tcu($vars) {
+function sarvaka_mediabase_transcripts_apachesolr_link_tcu($vars) {
+    $mins = floor ($vars['element']['#time'] / 60);
+    $secs = $vars['element']['#time'] % 60;
+    $time = sprintf ("%d:%02d", $mins, $secs);
+    $out = "<a href='" . $vars['element']['#linkurl'] . "' class='btn btn-default' role='button'>";
+    $out .= "<span class='glyphicon glyphicon-play'></span> ";
+    $out .= $time;
+    if (isset($vars['element']['#text'])) {
+        $out .= " " . $vars['element']['#text'];
+    }
+    $out .= "</a>";
+    return $out;
+}
+function sarvaka_mediabase_transcripts_ui_play_tcu($vars) {
         $mins = floor ($vars['element']['#time'] / 60);
         $secs = $vars['element']['#time'] % 60;
         $time = sprintf ("%d:%02d", $mins, $secs);
