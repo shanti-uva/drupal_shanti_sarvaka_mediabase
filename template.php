bcf712abd2db9404244befc31e5a0dff77157c6b
mferrara <mf8yk@virginia.edu>
2015-01-28 18:08:39 -0600

    remove test row

--- template.php
+++ template.php
@@ -346,8 +346,6 @@ function sarvaka_mediabase_transcripts_ui_transcript_navigation($vars) {
 	$out .= "<button type='button' class='btn btn-default btn-icon sameagain' title='Same line'><span class='icon shanticon-spin3'></span></button>";
 	$out .= "<button type='button' class='btn btn-default btn-icon next' title='Next line'><span class='icon shanticon-arrow-right'></span></button>";
 	$out .= "<button type='button' class='btn btn-default btn-icon searchtrans' title='Search Transcript'><span class='icon shanticon-magnify'></span></button>";
-	$out .= "</div>"
-	$out .= "<div>"
 	return $out;
 }
 function sarvaka_mediabase_transcripts_ui_transcript_search($vars) {
