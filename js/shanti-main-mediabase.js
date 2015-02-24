3a1e720e88b28f7c7917f04512020f8ea58902af
ThanGrove <than@virginia.edu>
2015-02-24 07:44:31 -0500

    implementing .once in shantiSarvakaMbMarkupTweaks so icons and labels don't get added multiple times

--- js/shanti-main-mediabase.js
+++ js/shanti-main-mediabase.js
@@ -14,11 +14,11 @@
 	Drupal.behaviors.shantiSarvakaMbMarkupTweaks = {
 		attach: function (context, settings) {
 			if(context == window.document) {
-				$('#edit-group-audience .form-item-group-audience-und').wrapInner('<div class="collection-details-audience"></div>');
-				$('.collection-details-audience').before($('.collection-details-audience > label').detach());
-				$('#edit-group-audience .form-item-group-audience-und > label, #edit-field-subcollection > label').prepend('<span class="icon shanticon-create"></span> ');
-				$('#edit-field-characteristic > label').prepend('<span class="icon shanticon-subjects"></span> ');
-				$('#edit-field-pbcore-coverage-spatial > label').prepend('<span class="icon shanticon-places"></span> ');
+				$('#edit-group-audience .form-item-group-audience-und', context).once('aveditcollaud').wrapInner('<div class="collection-details-audience"></div>');
+				$('.collection-details-audience').once('aveditcollaud2').before($('.collection-details-audience > label').detach());
+				$('#edit-group-audience .form-item-group-audience-und > label, #edit-field-subcollection > label').once('aveditsubcoll').prepend('<span class="icon shanticon-create"></span> ');
+				$('#edit-field-characteristic > label').once('aveditsubjects').prepend('<span class="icon shanticon-subjects"></span> ');
+				$('#edit-field-pbcore-coverage-spatial > label').once('aveditspatial').prepend('<span class="icon shanticon-places"></span> ');
 			}
 	  }
 	};
