b58f4ded9414dd355210315d6825351bacdc239a
ThanGrove <than@virginia.edu>
2015-04-10 09:33:45 -0400

    update to comment

--- js/mb-iframe.js
+++ js/mb-iframe.js
@@ -8,7 +8,7 @@
 	    }
 	}
 	
-	/** Check to see if cookie is set to be in iframe and whether this matches current state, if not reload outside of iframe **/
+	/** Check to see whether is in iframe and change class accordingly **/
 	Drupal.behaviors.sarvaka_mb_iframe_links = {
 		attach: function (context, settings) {
 			if(context == window.document) {
