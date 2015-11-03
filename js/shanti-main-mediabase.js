2b77431cc6e95eb071a630887dee21b509d24ec8
dukarla <artesano@vpn-user-1-249.itc.Virginia.EDU>
2015-11-03 16:21:33 -0600

    test hiding shanti-filters onLoad

--- js/shanti-main-mediabase.js
+++ js/shanti-main-mediabase.js
@@ -111,6 +111,13 @@
 					}
 				}
 	
+
+		// --- unhiding shanti-filters: inline styles keeps the default dropdown from flashing onLoad before the bootstrap-select script/css loads
+	Drupal.behaviors.shantiFiltersOnLoadFlickerControl = {
+	  attach: function (context, settings) {
+			$('.control-box-cell-filters")').show( "fast" );
+	  }
+	};
 				// Description Trimming
 				/* This makes there be multiple "Show More"s on Dreams page
 					Could perhaps use } else { if needed for other situations
