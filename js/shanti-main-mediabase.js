4cd25217cc76e861ad18a5795ec3afbf5754af21
ThanGrove <than@virginia.edu>
2015-11-18 14:17:47 -0500

    adding context detection to updating player size MANU-2046

--- js/shanti-main-mediabase.js
+++ js/shanti-main-mediabase.js
@@ -161,23 +161,27 @@
 	
 	Drupal.behaviors.shantiAVVideoFix = {
 		attach: function(context, settings) {
-			$('.kWidgetIframeContainer.kaltura-embed-processed').once('videosizeadjustment', function() {
-				$('.kWidgetIframeContainer.kaltura-embed-processed').prev('div').remove();
-				var ratio = Drupal.settings.mediabase.vratio,
-					  width = (ratio == '4:3') ? 520 : 667,
-					  height = 425,
-					  maxwidth = (ratio == '4:3') ? 550 : 720;
-				$('.kWidgetIframeContainer.kaltura-embed-processed').css({
-						'position':'', 
-						'top':'', 
-						'left': '', 
-						'right':'', 
-						'bottom':'', 
-						'width': width + 'px', 
-						'height': height+ 'px'
+			if (context == document) { 
+				$('.kWidgetIframeContainer.kaltura-embed-processed').once('videosizeadjustment', function() {
+					$('.kWidgetIframeContainer.kaltura-embed-processed').prev('div').remove();
+					$('.kWidgetIframeContainer.kaltura-embed-processed iframe').on('load', function() { 
+						var ratio = Drupal.settings.mediabase.vratio,
+							  width = (ratio == '4:3') ? 520 : 667,
+							  height = 425,
+							  maxwidth = (ratio == '4:3') ? 550 : 720;
+						$('.kWidgetIframeContainer.kaltura-embed-processed').css({
+								'position':'', 
+								'top':'', 
+								'left': '', 
+								'right':'', 
+								'bottom':'', 
+								'width': width + 'px', 
+								'height': height+ 'px'
+						});
+						$('.kWidgetIframeContainer.kaltura-embed-processed').parent().css('max-width', maxwidth + 'px'); 
+					});
 				});
-				$('.kWidgetIframeContainer.kaltura-embed-processed').parent().css('max-width', maxwidth + 'px'); 
-			});
+			}
 		}
 	};
 
