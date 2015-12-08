98c90ef02f297bab5c22857b1b182cbdb5614b7c
ThanGrove <than@virginia.edu>
2015-12-08 09:13:26 -0500

    removing console messages

--- js/shanti-search-mediabase.js
+++ js/shanti-search-mediabase.js
@@ -4,10 +4,8 @@
 	Drupal.behaviors.shanti_sarvaka_mb_search_input_update = {
 		attach: function (context, settings) {
 			if (context == document) {
-				console.log('here');
 				$("div.form-type-textfield input.form-text").each(function() {
-					console.log($(this).attr('placeholder'));
-					if ($(this).attr('placeholder') == "") {
+					if ($(this).attr('placeholder') == "undefined" || $(this).attr('placeholder') == "") {
 						$(this).attr('placeholder', 'Enter Search');
 					}
 				});
