566a3dda7d11ea017f34dbe02e1451b4084b1e6c
ThanGrove <than@virginia.edu>
2016-04-08 20:01:38 -0400

    adding template for collections list and mode changes

--- templates/views-view-unformatted--collections.tpl.php
+++ templates/views-view-unformatted--collections.tpl.php
@@ -1,19 +1,18 @@
 
- <?php
+<?php
 
 /**
  * @file
  * Default simple view template to display a list of rows.
  *
  * @ingroup views_templates
- * 
- * This was named views-views--media-by-kmap.tpl.php, but want to take out wrapping divs for browse-media 
- *  as well so making this more general (2015-10-28, ndg)
  */
 ?>
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
+  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
     <?php print $row; ?>
+  </div>
 <?php endforeach; ?>
