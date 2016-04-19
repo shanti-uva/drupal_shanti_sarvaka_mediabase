6d16af711029f65924ee5bbb5a75a2a4de2b43c7
dukarla <artesano@vpn-user-1-131.itc.Virginia.EDU>
2016-04-19 15:54:09 -0500

    testing equal-height script

--- js/shanti-main-mediabase.js
+++ js/shanti-main-mediabase.js
@@ -205,13 +205,13 @@
 	    if(context == window.document) {        
 
 	        $( document ).ready(function() {
-	          	$('.front.logged-in .equal-height,.node-type-collection .equal-height,.page-search .equal-height').delay(10000).matchHeight({
+	          	$('.front.logged-in .equal-height,.node-type-collection .equal-height,.page-search .equal-height').matchHeight({
 	              target: $('.equal-height.col-md-9')
 	          	});
 	        });
 
 	        $( document ).ready(function() {
-	          	$('.has-transcript .av-main-wrapper > div.col-xs-12').delay(10000).matchHeight({
+	          	$('.has-transcript .av-main-wrapper > div').matchHeight({
 	              target: $('.av-main-video-section')
 	          	});
 	        });
@@ -219,7 +219,7 @@
             $('.has-transcript .show-more a').bind('click', function(e){
             	e.preventDefault();
 
-        		$('.av-main-wrapper > div.col-xs-12').delay(10000).matchHeight({ 
+        		$('.av-main-wrapper > div').matchHeight({ 
         			target: $('.av-main-video-section') 
         		}); 
             }); 
