95fd51d2cfa07e5d91646f991c71930df233d88d
dukarla <artesano@vpn-user-1-138.itc.Virginia.EDU>
2016-04-29 07:27:24 -0500

    update equal-height

--- js/shanti-main-mediabase.js
+++ js/shanti-main-mediabase.js
@@ -205,9 +205,10 @@
 	    if(context == window.document) {        
 
 	        $( document ).ready(function() {
-	          	$('.front.logged-in .equal-height,.node-type-collection .equal-height,.page-search .equal-height').matchHeight({
+	          	$('.front.logged-in .equal-height,.node-type-collection .equal-height,.page-search .equal-height').once().matchHeight({
 	              target: $('.main-wrapper')
 	          	});
+	          	return false;
 	        });
 
 	        $( document ).ready(function() {
