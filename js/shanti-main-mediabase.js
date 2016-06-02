38edeb32141a70539d766b1d618af06576f43e98
dukarla <artesano@vpn-user-1-36.itc.Virginia.EDU>
2016-06-02 13:16:12 -0500

    add page-node-edit equal-height script

--- js/shanti-main-mediabase.js
+++ js/shanti-main-mediabase.js
@@ -245,7 +245,7 @@
 
 	        $( document ).ready(function() {
 	        	// $('.front.logged-in .equal-height,.node-type-collection .equal-height,.page-search .equal-height').matchHeight({
-	          	$('.front.logged-in .equal-height, .page-search .equal-height').matchHeight({
+	          	$('.front.logged-in .equal-height, .page-search .equal-height, page-node-edit.sidebar-second .equal-height').matchHeight({
 	              target: $('.wrap-all')
 	          	});
 	        });
